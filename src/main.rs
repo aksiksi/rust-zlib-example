97f090f75e423fe2f3c39bf41f547c21a052ad6a
Assil Ksiksi <cyph0nik@gmail.com>
2019-12-11 23:59:52 -0500

    Addd a basic compress/decompress test

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,25 @@ mod zlib;
 
 fn main() {
     println!("Hello, world!");
+}
 
-    let in_string = &mut String::from("Hello, world!");
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let mut out_arr = vec![0 as u8; in_string.len()];
-    let out_ptr = out_arr.as_mut_ptr();
+    #[test]
+    fn compress_then_decompress() {
+        let mut in_string = String::from("Hello, world!");
+        let in_ptr = in_string.as_mut_str().as_mut_ptr();
 
-    // Build empty stream struct
-    let defstream = zlib::z_stream::default();
+        let mut out_arr = vec![0 as u8; 64];
+        let out_ptr = out_arr.as_mut_ptr();
 
-    unsafe {
-        let in_ptr = in_string.as_bytes_mut().as_mut_ptr();
+        let mut final_arr = vec![0 as u8; in_string.len()];
+        let final_ptr = final_arr.as_mut_ptr();
+
+        // Build empty stream struct
+        let defstream = zlib::z_stream::default();
 
         // Update with required inputs
         let mut defstream = zlib::z_stream {
@@ -23,17 +31,34 @@ fn main() {
             ..defstream
         };
 
-        zlib::deflateInit_(&mut defstream,
-                           zlib::Z_BEST_COMPRESSION as i32,
-                           zlib::ZLIB_VERSION.as_ptr() as *mut i8,
-                           std::mem::size_of::<zlib::z_stream>() as i32);
-        zlib::deflate(&mut defstream, zlib::Z_FINISH as i32);
-        zlib::deflateEnd(&mut defstream);
-    }
+        // Compress
+        unsafe {
+            zlib::deflateInit_(&mut defstream,
+                               zlib::Z_BEST_COMPRESSION as i32,
+                               zlib::ZLIB_VERSION.as_ptr() as *mut i8,
+                               std::mem::size_of::<zlib::z_stream>() as i32);
+            zlib::deflate(&mut defstream, zlib::Z_FINISH as i32);
+            zlib::deflateEnd(&mut defstream);
+        }
 
-    println!("{}", in_string);
+        // Input is now the compressed output from above
+        let mut defstream = zlib::z_stream {
+            avail_in: out_arr.len() as u32,
+            next_in: out_ptr,
+            avail_out: final_arr.len() as u32,
+            next_out: final_ptr,
+            ..defstream
+        };
 
-    for c in out_arr {
-        println!("Char: {}", c);
+        // Decompress
+        unsafe {
+            zlib::inflateInit_(&mut defstream,
+                               zlib::ZLIB_VERSION.as_ptr() as *mut i8,
+                               std::mem::size_of::<zlib::z_stream>() as i32);
+            zlib::inflate(&mut defstream, zlib::Z_FINISH as i32);
+            zlib::inflateEnd(&mut defstream);
+        }
+
+        assert_eq!(&in_string, &std::str::from_utf8(&final_arr).unwrap());
     }
 }
